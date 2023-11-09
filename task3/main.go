e5fa77fd12aa49bcb01d1e641fb9945cbf1ba36f
Maxim Karasev <mxkrsv@disroot.org>
2023-11-09 17:45:14 +0300

    task3: use new application launch interface (NewApplication + Run)

--- task3/main.go
+++ task3/main.go
@@ -1,29 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"os"
-
 	"github.com/mxkrsv/etu-oop-2023/task3/application/cli"
+	//"github.com/mxkrsv/etu-oop-2023/task3/application/gui"
 	"github.com/mxkrsv/etu-oop-2023/task3/numbers"
 )
 
+type application interface {
+	Run()
+}
+
 func main() {
-	a := cli.NewApplication[int32, *numbers.Rational[int32]]()
-	a.PrintUsage()
-
-	s := bufio.NewScanner(os.Stdin)
-	os.Stdout.WriteString("> ")
-	for s.Scan() {
-		err := a.DispatchCommand(s.Text())
-		if err != nil {
-			panic(err)
-		}
-
-		os.Stdout.WriteString("> ")
-	}
-
-	if s.Err() != nil {
-		panic(s.Err())
-	}
+	var a application = cli.NewApplication[int32, *numbers.Rational[int32]]()
+	a.Run()
 }
